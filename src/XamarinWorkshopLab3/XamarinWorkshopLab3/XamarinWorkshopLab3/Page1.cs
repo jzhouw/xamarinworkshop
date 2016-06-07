2053879dbc1fe611108c45d57c6068a0a3dbab52
jameszhou <jameszhou@highviewsoftware.com>
2016-06-07 12:30:24 +0800

    lab 3

---
+++ src/XamarinWorkshopLab3/XamarinWorkshopLab3/XamarinWorkshopLab3/Page1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinWorkshopLab3
+{
+	public class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+            Button button = new Button { Text = "获取" };
+            Xamarin.Forms.Label lblWeather = new Xamarin.Forms.Label { Text = "当前天气" };
+            Xamarin.Forms.Label lblDevice = new Xamarin.Forms.Label { Text = "设备信息" };
+
+            button.Clicked += async delegate
+            {
+                MyClass myClass = new MyClass();
+                lblWeather.Text = await myClass.GetWeather();
+                lblDevice.Text = myClass.GetDevice();
+            };
+            
+
+			Content = new StackLayout {
+				Children = {
+                    button ,
+                    lblWeather,
+                    lblDevice
+                }
+			};
+		}
+	}
+}
---
+++ src/XamarinWorkshopLab3/XamarinWorkshopLab3/XamarinWorkshopLab3/WeatherInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinWorkshopLab3
+{
+    public class WeatherInfo
+    {
+        public MainInfo main { get; set; }
+    }
+    
+    public class MainInfo
+    {
+        public double temp { get; set; }
+
+        public double humidity {get;set;}
+    }
+}
