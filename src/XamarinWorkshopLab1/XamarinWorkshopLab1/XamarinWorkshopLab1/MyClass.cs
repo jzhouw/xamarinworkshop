220c3cacb4a159b18133afe2c5c33031fb579989
jameszhou <jameszhou@highviewsoftware.com>
2016-06-08 15:19:57 +0800

    use local data

--- src/XamarinWorkshopLab1/XamarinWorkshopLab1/XamarinWorkshopLab1/MyClass.cs
+++ src/XamarinWorkshopLab1/XamarinWorkshopLab1/XamarinWorkshopLab1/MyClass.cs
@@ -14,15 +14,15 @@ namespace XamarinWorkshopLab1
             string city = "beijing";
 
             //如下代码需要Internet网络
-            string apiKey = "1f05b04d2ec44ac5f74dffe877e4234d"; //http://openweathermap.org/api
-            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={apiKey}";
-            System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
-            string jsonWeather = await httpClient.GetStringAsync(apiUrl);
-            WeatherInfo weather = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherInfo>(jsonWeather);
-            return $"{city} 气温: {weather.main.temp}, 湿度: {weather.main.humidity}";
+            //string apiKey = "1f05b04d2ec44ac5f74dffe877e4234d"; //http://openweathermap.org/api
+            //string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={apiKey}";
+            //System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+            //string jsonWeather = await httpClient.GetStringAsync(apiUrl);
+            //WeatherInfo weather = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherInfo>(jsonWeather);
+            //return $"{city} 气温: {weather.main.temp}, 湿度: {weather.main.humidity}";
 
             //没有Internet网络时,请使用如下代码
-            //return $"{city} 气温: 22, 湿度: 11";
+            return $"{city} 气温: 22, 湿度: 11";
         }
 
         public string GetDevice()
