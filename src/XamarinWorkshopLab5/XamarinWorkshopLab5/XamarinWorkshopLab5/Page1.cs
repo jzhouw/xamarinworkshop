ae82402e6bda43a006d854dd72798b2cb9867368
jameszhou <jameszhou@highviewsoftware.com>
2016-06-11 13:45:14 +0800

    added lab 5

---
+++ src/XamarinWorkshopLab5/XamarinWorkshopLab5/XamarinWorkshopLab5/Page1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinWorkshopLab5
+{
+    public class Page1:ContentPage
+    {
+        public Page1()
+        {
+            Button button = new Button { Text = "Go to Page 2" };
+            
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label { Text = "Page 1" },  
+                    button
+                }
+            };
+
+            button.Clicked += delegate
+            {
+                Navigation.PushAsync(new Page2());
+            };
+        }
+
+    }
+}
---
+++ src/XamarinWorkshopLab5/XamarinWorkshopLab5/XamarinWorkshopLab5/Page2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinWorkshopLab5
+{
+    public class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            Button button = new Button { Text = "Back" };
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Page 2" },
+                    button
+                }
+            };
+
+            button.Clicked += delegate
+            {
+                Navigation.PopAsync();
+            };
+        }
+    }
+}
